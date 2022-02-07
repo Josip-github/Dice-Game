e1871f9ac1a7cbf117085e23fdca2eaf031d4950
Josip-github <josipcota1@gmail.com>
2022-02-07 10:49:55 +0100

    create function winnerOfPlay

--- app.js
+++ app.js
@@ -3,3 +3,15 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 document.querySelectorAll("img")[0].setAttribute("src", "images/dice" + randomNumber1 + ".png");
 document.querySelectorAll("img")[1].setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+function winnerOfPlay(){
+
+    if(randomNumber1 > randomNumber2){
+        return "Player 1 wins!"
+    } else if(randomNumber1 < randomNumber2){
+        return "Player 2 wins!"
+    } else {
+        return "Draw";
+    }
+}
+
